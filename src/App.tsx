44d43054fae05f9f5e4267e7a78051abeb099034
Wat <gazentiadev8352@gmail.com>
2026-06-27 21:39:09 +0700

    feat: add Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import { Navbar } from "./components/Navbar"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <main>
-      <h1 className="text-accent">gazentia.dev</h1>
+      <Navbar />
     </main>
   )
 }
